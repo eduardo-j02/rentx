e6627487f7540220c822805cbc7f2b7eae91895b
Eduardo Silva <eduardosilva@MacBook.local>
2022-01-05 17:06:17 -0300

    chapter IV

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,13 +2,13 @@ import express, { Response, Request, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "./database";
+import "@shared/infra/typeorm";
 
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 
-import { router } from "./shared/infra/http/routes";
-import swaggerFile from "./swagger.json";
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
 
 const app = express();
 
